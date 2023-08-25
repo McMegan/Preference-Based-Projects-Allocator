2f86041ceaf2000272415199d0b871a4a515f3e4
Megan <100398893+McMegan@users.noreply.github.com>
2023-08-25 15:34:51 +1000

    Fix export file name

--- manager/export.py
+++ manager/export.py
@@ -1,3 +1,4 @@
+import datetime
 import csv
 from io import StringIO
 
@@ -16,22 +17,29 @@ def generate_allocation_results_csv(unit):
     writer.writerow(['student_id', 'project_number', 'project_name'])
     # Write students to file
     for student in unit.enrolled_students.all():
-        writer.writerow(
-            [student.student_id, student.assigned_project.number, student.assigned_project.name])
+        if student.assigned_project:
+            writer.writerow(
+                [student.student_id, student.assigned_project.number, student.assigned_project.name])
+        else:
+            writer.writerow(
+                [student.student_id, '', ''])
 
     file_output.seek(0)
     return file_output
 
 
+def generate_allocation_results_filename(unit):
+    return datetime.datetime.now().strftime('%Y%m%d-%H_%M_%S')+f'-{unit.code.lower()}-project-allocation.csv'
+
+
 def download_allocation_results_csv(unit_id):
     unit = models.Unit.objects.filter(pk=unit_id).prefetch_related('projects').prefetch_related(
         'enrolled_students').prefetch_related('enrolled_students__project_preferences').first()
-    filename = f'{unit.code}-project-allocation.csv'
 
     response = HttpResponse(
         content_type='text/csv',
         headers={
-            'Content-Disposition': f'attachment; filename="{filename}"'},
+            'Content-Disposition': f'attachment; filename="{generate_allocation_results_filename(unit)}"'},
         content=generate_allocation_results_csv(unit)
     )
 
@@ -41,7 +49,6 @@ def download_allocation_results_csv(unit_id):
 def email_allocation_results_csv(unit_id, manager_id):
     unit = models.Unit.objects.filter(pk=unit_id).prefetch_related('projects').prefetch_related(
         'enrolled_students').prefetch_related('enrolled_students__project_preferences').first()
-    filename = f'{unit.code}-project-allocation.csv'
 
     attachment = generate_allocation_results_csv(unit)
 
@@ -54,7 +61,8 @@ def email_allocation_results_csv(unit_id, manager_id):
             from_email=None,
             to=[manager.email]
         )
-        email.attach(filename, attachment.read(), 'text/csv')
+        email.attach(generate_allocation_results_filename(
+            unit), attachment.read(), 'text/csv')
         result = email.send(fail_silently=False)
         return 'Email successful' if result else 'Email failed'
     return 'No email specified'

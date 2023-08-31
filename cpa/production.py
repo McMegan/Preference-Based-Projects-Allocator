166aeea0e7921fd7284c716968c660cacd51ef0c
Megan <100398893+McMegan@users.noreply.github.com>
2023-08-31 19:20:10 +1000

    Update requirements

--- cpa/production.py
+++ cpa/production.py
@@ -9,6 +9,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # that Azure automatically creates for us.
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']
                  ] if 'WEBSITE_HOSTNAME' in os.environ else []
+
 CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']
                         ] if 'WEBSITE_HOSTNAME' in os.environ else []
 DEBUG = False
--- manage.py
+++ manage.py
@@ -6,7 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cpa.settings')
+    settings_module = "cpa.production" if 'WEBSITE_HOSTNAME' in os.environ else 'cpa.settings'
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:

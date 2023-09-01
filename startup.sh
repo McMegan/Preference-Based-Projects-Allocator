f551dc33d598967421b5f453e9ddf92968ed62c7
Megan <100398893+McMegan@users.noreply.github.com>
2023-09-01 17:40:20 +1000

    Fix startup.sh

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 export LANG=C.UTF-8
 
-celery --app=cpa worker -l INFO & gunicorn --bind=0.0.0.0 --timeout 600 config.wsgi
+celery --app=cpa worker -l INFO & gunicorn --bind=0.0.0.0 --timeout 600 cpa.wsgi

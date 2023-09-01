89504ed3de735e9828c80ec3aa2fbceeb8d2b6b5
Megan <100398893+McMegan@users.noreply.github.com>
2023-09-01 17:57:53 +1000

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 export LANG=C.UTF-8
 
-gunicorn --bind=0.0.0.0 --timeout 600 cpa.wsgi & celery --app=cpa worker -l INFO
+gunicorn --bind=0.0.0.0 --timeout 600 cpa.wsgi && celery --app=cpa worker -l INFO

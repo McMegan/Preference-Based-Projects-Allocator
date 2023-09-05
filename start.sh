6bf10cd3957b2f21f5b8fd8100700a763d0d600c
Megan <100398893+McMegan@users.noreply.github.com>
2023-09-05 12:05:29 +1000

    Fix port

--- start.sh
+++ start.sh
@@ -5,6 +5,5 @@ set -o pipefail
 set -o nounset
 
 python manage.py migrate
-# python manage.py runserver 0.0.0.0:8000
 
-gunicorn cpa.wsgi:application --bind 0.0.0.0:8000 --timeout 600 --preload --threads 2 -w 2 --log-level 'debug'
+gunicorn cpa.wsgi:application --bind 0.0.0.0:8080 --timeout 600 --preload --threads 2 -w 2 --log-level 'debug'

4b15b2b876ccb8ab9a2a49a5f851f270bd897b8e
Megan <100398893+McMegan@users.noreply.github.com>
2023-09-07 16:25:24 +1000

    Test gmail

--- cpa/settings.py
+++ cpa/settings.py
@@ -117,4 +117,9 @@ DJANGO_TABLES2_TEMPLATE = 'django_tables2/bootstrap5-responsive.html'
 
 
 # EMAIL BACKEND
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
